39a85b5970d4c094c64f861b001aee3313d0f86e
Kostya <koooooostaaa@gmail.com>
2023-01-29 12:01:56 +0200

    Init commit

---
+++ resources/js/components/PostComponent.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="container">
+        <table class="table">
+            <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Name</th>
+                <th scope="col">Age</th>
+                <th scope="col">Job</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr v-for="person in people" :key="person.id">
+                <th scope="row">{{ person.id }}</th>
+                <td>{{ person.name }}</td>
+                <td>{{ person.age }}</td>
+                <td>{{ person.job }}</td>
+            </tr>
+            </tbody>
+        </table>
+    </div>
+
+</template>
+
+<script>
+export default {
+    name: "PostComponent",
+    data() {
+        return {
+            people: [{id: null, name: null, age: null, job: null}]
+        }
+    },
+    methods: {
+        getPeople() {
+            axios.get('/people')
+                .then((res) => {
+                    this.people = res.data;
+                })
+                .catch((error) => {
+                    //
+                })
+        }
+    },
+    created() {
+        this.getPeople();
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
